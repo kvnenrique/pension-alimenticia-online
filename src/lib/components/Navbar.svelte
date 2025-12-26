a916630c198d4c65e994ccd6c5c1f0a48f65ca3a
Kevin Enrique Ortega Olvera <kevinenriqueortega98@gmail.com>
2025-12-26 14:40:16 -0600

    dev(feat): Navbar colors customized

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -3,16 +3,16 @@
     import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from "flowbite-svelte";
 </script>
 
-<Navbar class="bg-pink-900">
+<Navbar class="bg-4T">
     <NavBrand href="/">
         <!-- <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />  -->
-        <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">{STRINGS.appName}</span>
+        <span class="self-center text-xl font-semibold whitespace-nowrap text-white">{STRINGS.appName}</span>
     </NavBrand>
     <NavHamburger />
     <NavUl>
-        <NavLi href="/">{STRINGS.home}</NavLi>
-        <NavLi href="/about_us">{STRINGS.about_us}</NavLi>
-        <NavLi href="/privacy_policy">{STRINGS.privacy_policy}</NavLi>
-        <NavLi href="/contact">{STRINGS.contact}</NavLi>
+        <NavLi class="navlink" href="/">{STRINGS.home}</NavLi>
+        <NavLi class="navlink" href="/about_us">{STRINGS.about_us}</NavLi>
+        <NavLi class="navlink" href="/privacy_policy">{STRINGS.privacy_policy}</NavLi>
+        <NavLi class="navlink" href="/contact">{STRINGS.contact}</NavLi>
     </NavUl>
 </Navbar>
